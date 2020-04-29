8521315ac0763b756396fd3168de36891ffd7748
Sikkandar <sikkandarsulaiman@gmail.com>
2020-04-29 13:45:06 +0530

    field skeleton created

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ def main(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
-curses.wrapper(main)
+# curses.wrapper(main)
+
+
+def createField(width, height):
+	field = ['#'+' '*(width-2)+'#' for i in range(height-1)]
+	field.append('#'*width)
+	return field
+
+def getPiece():
+	return '*\n***'
+
+def showPieceOverField(field, piece, x, y):
+	pass
+
+for i in createField(20, 80):
+	print(i)
