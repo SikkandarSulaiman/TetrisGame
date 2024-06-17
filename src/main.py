32b2ce75328dc746cbe9306cf6868742718f6929
SikkandarSulaiman <sikkandarsulaiman@gmail.com>
2024-06-17 09:51:00 +0530

    Fixed fall down time issues

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import random
 import pygame
 
@@ -8,65 +9,36 @@ from panels import ScoreBoard
 from config_read import *
 from ui_gamescreen_pyg import pygame_GameScreen
 
-def cur_main(stdscr):
-	stdscr.clear()
-	stdscr.nodelay(1)
-	stdscr.timeout(INITIAL_SPEED_MS)
-	curses.curs_set(0)
-	
-	tetris_field = Field(FIELD_WIDTH, FIELD_HEIGHT)
-	pf = PieceFitter(tetris_field)
-	sb = ScoreBoard(SCOREBOARD_WIDTH, SCOREBOARD_HEIGHT)
-	ScoreManager().set_scoreboard(sb)
-
-	pf.spawn_new_piece()
-	
-	tetris_field.print_panel(stdscr)
-	pf.active_piece.print_piece_in_field(stdscr, tetris_field)
-	sb.print_panel(stdscr)
-
-	while True:
-
-		if not pf.frr: # check Field Refresh Request
-			key = stdscr.getch()
-			dir = input_key_map.get(key, CMD_QUIT)
-			if dir == CMD_QUIT:
-				break
-			pf.move_piece(dir)
-		elif pf.move_piece_refresh() == CMD_GAME_OVER:
-			break
-
-		tetris_field.print_panel(stdscr)
-		if pf.frr: continue
-		pf.active_piece.print_piece_in_field(stdscr, tetris_field)
-		sb.print_panel(stdscr)
-
-	stdscr.refresh()
-
-
 if __name__ == '__main__':
-
-	NO_KEY_PRESS = -1
-	input_key_map[NO_KEY_PRESS] = CMD_DOWN
 
 	scr = pygame_GameScreen()
 
 	tetris_field = Field(FIELD_WIDTH, FIELD_HEIGHT)
 	pf = PieceFitter(tetris_field)
-	sb = ScoreBoard(SCOREBOARD_WIDTH, SCOREBOARD_HEIGHT)
-	ScoreManager().set_scoreboard(sb)
+	# sb = ScoreBoard(SCOREBOARD_WIDTH, SCOREBOARD_HEIGHT)
+	# ScoreManager().set_scoreboard(sb)
 
 	pf.spawn_new_piece()
 	scr.print_panel(tetris_field)
 	# sb.print_panel(scr)
 
+	loop_end_time = 0
+	last_cmddown_time = 0
 	running = True
 	while running:
 
-		key_detected = scr.detect_input_keys(500)
+		# Record loop duration
+		loop_start_time = time.time()
+		last_cmddown_time += (loop_end_time)
+
+		# Read input
+		key_detected = scr.detect_input_keys(INITIAL_SPEED_MS)
+
+		# Process input
 		if not pf.frr: # check Field Refresh Request
-			if not key_detected:
+			if last_cmddown_time >= INITIAL_SPEED_MS:
 				pf.move_piece(CMD_DOWN)
+				last_cmddown_time = 0
 			for k,v in scr.key_map.items():
 				if v:
 					if k == CMD_QUIT:
@@ -78,10 +50,14 @@ if __name__ == '__main__':
 			print('GAME OVER DETECTED')
 			break
 
+		# Update output
 		scr.print_panel(tetris_field)
 		if pf.frr: continue
 		scr.print_piece_over_panel(pf.active_piece, tetris_field)
 		scr.refresh()
+		
+		# update loop duration
+		loop_end_time = (time.time() - loop_start_time)*1000
 
 	scr.quit()
 
